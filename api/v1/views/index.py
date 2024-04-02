62f0e5b8df9cc25b146494368dba04f27075f183
Jeanluchabi <jeanuchabi@gmail.com>
2024-04-02 09:57:05 +0200

    updated index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,11 +9,29 @@ from api.v1 import app_views
 # Import Flask jsonify
 from flask import jsonify
 
-# This creates route for status endpoint
+# Import storage
+from models import storage
+
+# Create route for status endpoint
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
     Return status: OK
     """
     return jsonify({"status": "OK"})
+
+# Create route for stats endpoint
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    Return the number of objects by type
+    """
+    stats_dict = {}
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+
+    for cls_name in classes:
+        cls_count = storage.count(cls_name)
+        stats_dict[cls_name] = cls_count
+
+    return jsonify(stats_dict)
 
